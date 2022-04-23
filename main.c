931c9b2732bd1c38992dc0b98d16b6d8e5b2f12b
Chris-Haj <chrishaj1191@gmail.com>
2022-04-23 18:53:52 +0300

    so much fixing

--- main.c
+++ main.c
@@ -25,10 +25,16 @@ void counter(const char *line, size_t i);
 void readHistory(FILE *file);
 
 void cmdFromHistory(char *line);
+
 void cmdSplitter(const char *line, int pipeAmount, int *wordsAmount, const int *pipeIndexes);
+
 void executeOneCmd(char *cmd[]);
+
 void executeTwoCmds(char *cmd[], char *cmd2[]);
+
 void executeThreeCmds(char *cmd[], char *cmd2[], char *cmd3[]);
+
+void test();
 
 int numberOfCommands = 1;
 int numberOfPipes = 0;
@@ -38,12 +44,180 @@ int running = 1;
 int main() {
 //    loop();
 
-    char *test[] = {"ls", "-l", NULL};
-    char *other[] = {"sort",NULL};
-    executeTwoCmds(test,other);
-//    executeOneCmd(test);
-
+    char *tes[] = {"xzx","-l", NULL};
+    char *other[] = {"brr", NULL};
+    char *last[] = {"arr", NULL};
+    executeOneCmd(tes);
+//    executeThreeCmds(tes, other, last);
     return 0;
+
+}
+
+void executeOneCmd(char *cmd[]) {
+    char *success = "111";
+    char *fail = "000";
+    int fd[2];
+    pipe(fd);
+    int status;
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("fork() failed");
+        exit(0);
+    } else if (pid == 0) {
+        dup2(fd[1], STDOUT_FILENO);
+        close(fd[0]);
+        close(fd[1]);
+        char res[2];
+        if (execvp(*cmd, cmd) == -1) {
+            strcpy(res, fail);
+        } else
+            strcpy(res, success);
+        write(1, res, strlen(success) + 1);
+    } else {
+        dup2(fd[0], STDIN_FILENO);
+        close(fd[0]);
+        close(fd[1]);
+        char res[2];
+        read(0, res, strlen(success) + 1);
+        printf("%s\n",res);
+    }
+    wait(&status);
+}
+
+void executeTwoCmds(char *cmd[], char *cmd2[]) {
+    int fd[2];
+    pipe(fd);
+    pid_t pid = fork();
+    int status;
+    int suc;
+    if (pid == 0) {
+        dup2(fd[1], STDOUT_FILENO);
+        close(fd[0]);
+        close(fd[1]);
+        execvp(*cmd, cmd);
+    } else {
+        pid = fork();
+        if (pid == 0) {
+            close(fd[1]);
+            dup2(fd[0], STDIN_FILENO);
+            execvp(*cmd2, cmd2);
+            close(fd[1]);
+        }
+
+    }
+    close(fd[0]);
+    close(fd[1]);
+    wait(&status);
+    wait(&status);
+}
+
+void executeThreeCmds(char *cmd[], char *cmd2[], char *cmd3[]) {
+
+    int fd[4];
+    int status;
+    pipe(fd);
+    pipe(fd + 2);
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("fork() error");
+        for (int i = 0; i < 3; i++)
+            close(fd[i]);
+        exit(1);
+    } else if (pid == 0) {
+        dup2(fd[1], 1);
+        close(fd[0]);
+        close(fd[1]);
+        close(fd[2]);
+        close(fd[3]);
+        execvp(*cmd, cmd);
+    } else {
+        pid = fork();
+        if (pid == -1) {
+            perror("fork() error");
+            for (int i = 0; i < 3; i++)
+                close(fd[i]);
+            exit(1);
+        } else if (pid == 0) {
+            dup2(fd[0], 0);
+            dup2(fd[3], 1);
+            close(fd[0]);
+            close(fd[1]);
+            close(fd[2]);
+            close(fd[3]);
+            execvp(*cmd2, cmd2);
+        } else {
+            pid = fork();
+            if (pid == -1) {
+                perror("fork() error");
+                for (int i = 0; i < 3; i++)
+                    close(fd[i]);
+                exit(1);
+            } else if (pid == 0) {
+                dup2(fd[2], 0);
+                close(fd[0]);
+                close(fd[1]);
+                close(fd[2]);
+                close(fd[3]);
+                execvp(*cmd3, cmd3);
+            }
+        }
+    }
+    close(fd[0]);
+    close(fd[1]);
+    close(fd[2]);
+    close(fd[3]);
+    for (int i = 0; i < 3; i++)
+        wait(&status);
+}
+
+void test() {
+    int status;
+    int i;
+
+    // arguments for commands; your parser would be responsible for
+    // setting up arrays like these
+
+    char *cat_args[] = {"ls", NULL};
+    char *grep_args[] = {"sort", NULL};
+    char *cut_args[] = {"wc", NULL};
+    int pipes[4];
+    pipe(pipes); // sets up 1st pipe
+    pipe(pipes + 2); // sets up 2nd pipe
+    if (fork() == 0) {
+        dup2(pipes[1], 1);
+        close(pipes[0]);
+        close(pipes[1]);
+        close(pipes[2]);
+        close(pipes[3]);
+        execvp(*cat_args, cat_args);
+    } else {
+        if (fork() == 0) {
+            dup2(pipes[0], 0);
+            dup2(pipes[3], 1);
+            close(pipes[0]);
+            close(pipes[1]);
+            close(pipes[2]);
+            close(pipes[3]);
+            execvp(*grep_args, grep_args);
+        } else {
+            if (fork() == 0) {
+                dup2(pipes[2], 0);
+                close(pipes[0]);
+                close(pipes[1]);
+                close(pipes[2]);
+                close(pipes[3]);
+
+                execvp(*cut_args, cut_args);
+            }
+        }
+    }
+    close(pipes[0]);
+    close(pipes[1]);
+    close(pipes[2]);
+    close(pipes[3]);
+
+    for (i = 0; i < 3; i++)
+        wait(&status);
 }
 
 //Main loop function to keep asking user for input and call other functions according to what is passed into the stdin stream
@@ -145,7 +319,7 @@ void counter(const char *line, size_t i) {
     numberOfCommands++;
     for (int c = 0; c < curPipe + 1; c++)
         totalNumberOfWords += wordsAmount[c];
-    cmdSplitter(line,curPipe,wordsAmount,pipeIdx);
+    cmdSplitter(line, curPipe, wordsAmount, pipeIdx);
 }
 
 /*
@@ -193,8 +367,8 @@ void cmdSplitter(const char *line, int pipeAmount, int *wordsAmount, const int *
     int cmdAmount = pipeAmount + 1;
     char **commands[cmdAmount];
     for (; i < cmdAmount; i++) {
-        commands[i] = (char **) calloc(wordsAmount[i]+1, sizeof(char **));
-        commands[i][wordsAmount[i]]=NULL;
+        commands[i] = (char **) calloc(wordsAmount[i] + 1, sizeof(char **));
+        commands[i][wordsAmount[i]] = NULL;
     }
     i = 0;
     for (int c = 0; c < cmdAmount; c++) {
@@ -221,103 +395,21 @@ void cmdSplitter(const char *line, int pipeAmount, int *wordsAmount, const int *
             }
         }
         if (c < pipeAmount)
-            i = pipeIndexes[c]+1;
+            i = pipeIndexes[c] + 1;
         index = 0;
     }
-    if(cmdAmount==1)
+    if (cmdAmount == 1)
         executeOneCmd(commands[0]);
-    else if(cmdAmount==2)
-        executeTwoCmds(commands[0],commands[1]);
+    else if (cmdAmount == 2)
+        executeTwoCmds(commands[0], commands[1]);
     else
-        executeThreeCmds(commands[0],commands[1],commands[2]);
+        executeThreeCmds(commands[0], commands[1], commands[2]);
     for (i = 0; i < cmdAmount; i++) {
-        for(int j = 0 ;j < wordsAmount[i]+1;j++){
+        for (int j = 0; j < wordsAmount[i] + 1; j++) {
             free(commands[i][j]);
         }
         free(commands[i]);
     }
 }
-void executeOneCmd(char *cmd[]){
-    int fd[2];
-    pipe(fd);
-    pid_t pid = fork();
-    int commandStatus;
-    if (pid == 0) {
-        close(fd[0]);
-        commandStatus = execvp(*cmd, cmd);
-        dup2(fd[1], 1);
-        printf("%d", commandStatus);
-        close(fd[1]);
-        exit(0);
-    }
-    else {
-        close(fd[1]);
-        dup2(fd[0], 0);
-        int failed;
-        scanf("%d", &failed);
-        if (failed == -1)
-            fprintf(stderr,"cmd not found!\n");
-        close(fd[0]);
-    }
 
-}
-void executeTwoCmds(char *cmd[], char *cmd2[]){
-    int pipes = 4;
-    int fd[pipes];
-    pipe(fd);
-    pipe(fd+2);
-    pid_t pid = fork();
-    int status;
-    int commandStatus;
-    if(pid == 0){//first child (does first cmd and sends to child two)
-        close(fd[0]);
-        close(fd[2]);
-        close(fd[3]);
-        commandStatus = execvp(*cmd, cmd);
-        dup2(fd[1], 1);
-        printf("%d", commandStatus);
-        close(fd[1]);
-        exit(0);
-    }// fd[0] second child reads from first child
-    //  fd[1] first child writes to second child
-    //  fd[2] father reads from second child
-    //  fd[3] second child writes to father
-    else{ // father
-        pid = fork();//create second child
-        if(pid == 0){//second child (takes input from first child and sends to father
-            close(fd[1]);
-            dup2(fd[0],0);
-            int stat;
-            scanf("%d",&stat);
-            close(fd[0]);
-            if(stat == -1){
-                fprintf(stderr,"first command is unknown!\n");
-                close(fd[3]);
-                close(fd[2]);
-            }
-            dup2(fd[3],1);
-            commandStatus = execvp(*cmd2,cmd2);
-            printf("%d",commandStatus);
-            close(fd[2]);
-            close(fd[3]);
-            exit(1);
-        }
-        close(fd[0]);
-        close(fd[1]);
-        int stat;
-        dup2(fd[2],0);
-        scanf("%d",&stat);
-        if(stat == -1)
-            fprintf(stderr,"second command is unknown");
-        close(fd[2]);
-        close(fd[3]);
-       /* for(int i=0;i<2;i++)
-            wait(&status);*/
-        exit(0);
-    }
-}
-void executeThreeCmds(char *cmd[], char *cmd2[], char *cmd3[]){
-    int fd[6];
-
-}
 
