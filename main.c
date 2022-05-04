08f2a3625e47c152270b4b90c2a6a65982e55d7a
chris <chrishaj1191@gmail.com>
2022-05-05 01:39:51 +0300

    hang finished

--- main.c
+++ main.c
@@ -26,13 +26,13 @@ void executeTwoCmds(char **cmd[], int *words, int background);
 void executeThreeCmds(char **cmd[], int *words, int background);
 void freeCommands(char **cmd[], const int *words, int commandsAmnt);
 int fromHistoryLineToCmd(char *line, size_t i);
-void handler(){
-    int status;
-    waitpid(-1,&status,WNOHANG);
+void handler(int sig){
+    int stat=1;
+    while(stat!=-1&&stat!=0)
+        stat = waitpid(-1,&sig,WNOHANG);
 }
 
 int numberOfCommands = 1;
-int numberOfPipes = 0;
 int totalNumberOfWords = 0;
 int running = 1;
 
@@ -42,6 +42,13 @@ int main() {
 }
 
 int fromHistoryLineToCmd(char *line, size_t i) {
+    /*
+     * this function is used to check if there are !x commands which are stored in the history file,
+     * a loop is used to go over the array to split the input into commands depending on how many pipes exist
+     * ,while separating the commands, if the command is of type !x then the number x is saved into historyLines array
+     * into the fitting index depending on if it is the first/second/third command, if the command is in word format then its index
+     * in historyLines is equal -1.
+     */
     int historyLines[] = {-1, -1, -1};
     int beginningOfCmd[] = {0, 0, 0};
     int indexes[] = {-1, -1};
@@ -65,8 +72,10 @@ int fromHistoryLineToCmd(char *line, size_t i) {
         }
         i++;
     }
+    //If all commands were in normal word format then return 0 and do nothing to the input and continue normal execution
     if (historyLines[0] == -1 && historyLines[1] == -1 && historyLines[2] == -1)
         return 0;
+    //If we found more than 2 pipes than return 1 meaning input is not valid for execution.
     else if (cur > 2)
         return 1;
     else {
@@ -84,9 +93,12 @@ int fromHistoryLineToCmd(char *line, size_t i) {
 int cmdFromHistory(char *line, const int linesNum[], const int size, const int max, const int pipeIndexes[], const int beginningOfCmd[]) {
     FILE *file = fopen(FILENAME, "r");
     char command[size][LENGTH];
-    for (int j = 0; j < size; j++)
+    for (int j = 0; j < size; j++) //empty all strings
         strcpy(command[j], "\0");
     char curLine[LENGTH];
+    /*
+     * Any command that was originally in the initial input in words format it which is indicated at by a -1 in the linesNum array index
+     * is copied into the matching index into the command array*/
     for (int c = 0; c < size; c++) {
         if (linesNum[c] == -1) {
             if (pipeIndexes[c] != -1 && c < 2) {
@@ -98,6 +110,7 @@ int cmdFromHistory(char *line, const int linesNum[], const int size, const int m
         }
     }
     int cur = 1;
+    //pass over the lines from the history file and copying the lines we need into the commands array
     while (fgets(curLine, LENGTH, file)) {
         if (cur == linesNum[0]) {
             strcpy(command[0], curLine);
@@ -114,6 +127,7 @@ int cmdFromHistory(char *line, const int linesNum[], const int size, const int m
             break;
     }
     fclose(file);
+    //If cur does not reach max then that means one or more of the commands' number (!x) did not yet exist in the file
     if (cur < max) {
         fprintf(stderr, "One of the numbers entered does not exist in the file yet\n");
         return 1;
@@ -121,6 +135,7 @@ int cmdFromHistory(char *line, const int linesNum[], const int size, const int m
     strcpy(curLine, "");
     printf("%s\n", command[0]);
     printf("%s\n", command[1]);
+    //After reformatting the commands from !x to words they are combined again to be sent as a normal command to parse and execute
     if (size == 1) {
         sprintf(line, "%s\n", command[0]);
     } else if (size == 2) {
@@ -129,6 +144,9 @@ int cmdFromHistory(char *line, const int linesNum[], const int size, const int m
         sprintf(line, "%s|%s|%s\n", command[0], command[1], command[2]);
     }
     int counter = 0;
+    /*In case one of the past commands had a pipe in it and the total amount of pipes reached is more than 2 then return 1
+     * ,meaning command is not valid for execution.
+     */
     for (int c = 0; line[c] != '\n'; c++)
         if (line[c] == '|')
             counter++;
@@ -143,11 +161,14 @@ void cmdSplitter(const char *line, int pipeAmount, int *wordsAmount, const int *
     int start = 0, end, index = 0;
     int cmdAmount = pipeAmount + 1;
     char **commands[cmdAmount];
+    //Each array is allocated memory according to how many words each command has while also making the last pointer of each array point to NULL
     for (; i < cmdAmount; i++) {
         commands[i] = (char **) calloc(wordsAmount[i] + 1, sizeof(char **));
         commands[i][wordsAmount[i]] = NULL;
     }
     i = 0;
+    /*Each array inside the commands[] array is allocated memory to copy each word of the current command in a separate index and after
+     * reaching the end of a command which we know by finding a | then the next command is copied into the next array's indexes*/
     for (int c = 0; c < cmdAmount; c++) {
         for (; line[i] != '\n' && line[i] != '|'; i++) {
             if (line[i] == ' ' || line[i + 1] == '\n' || line[i + 1] == '|') {
@@ -166,8 +187,8 @@ void cmdSplitter(const char *line, int pipeAmount, int *wordsAmount, const int *
                     index++;
                 }
                 start = end;
-                while (line[start] == ' ') start++;
-
+                while (line[start] == ' ')
+                    start++;
                 if (line[start] == '\n' || line[start] == '|') {
                     break;
                 }
@@ -208,18 +229,15 @@ void executeOneCmd(char **cmd[], int *words, int background) {
         freeCommands(cmd, words, 2);
         exit(1);
     } else if (pid == 0) {
-        if(background==1)
-            printf("\n%d\n",getpid());
         if (-1 == execvp(*(cmd[0]), cmd[0])) {
             perror("command not found");
             freeCommands(cmd, words, 1);
             exit(1);
         }
     }
+    //If the initial input was received normally with nohup and no ampersand then the father is told to wait the child
     if (background == 0)
         wait(&status);
-    else
-        waitpid(-1, &status, WNOHANG);
 }
 
 void executeTwoCmds(char **cmd[], int *words, int background) {
@@ -237,8 +255,6 @@ void executeTwoCmds(char **cmd[], int *words, int background) {
         freeCommands(cmd, words, 2);
         exit(1);
     } else if (pid == 0) {
-        if(background==1)
-            printf("\n%d\n",getpid());
         if (dup2(fd[1], 1) == -1) {
             perror("dup failure");
             close(fd[0]); /*--*/ close(fd[1]);
@@ -288,23 +304,25 @@ void executeTwoCmds(char **cmd[], int *words, int background) {
         freeCommands(cmd, words, 2);
         exit(1);
     }
+    //If the initial input was received normally with nohup and no ampersand then the father is told to wait for the two children processes
     if(background==0){
-        wait(&status);
-        wait(&status);
-    }
-    else{
-        waitpid(-1,&status,WNOHANG);
-        waitpid(-1,&status,WNOHANG);
+        for(int i=0;i<2;i++)
+            wait(&status);
     }
 }
 
 void executeThreeCmds(char **cmd[], int *words, int background) {
     int fd[4];
     int status;
+    /*Array fd is used to open two pipes at index [0,1] and [2,3] which are used later for all three children created via fork
+     * to pass in the input and output of each command from one to the next */
     if (pipe(fd) == -1 || pipe(fd + 2) == -1) {
         perror("pipe failure");
         exit(1);
     }
+    /*fork is used 3 times to create 3 children processes, for each child to run one command while give its output as input
+     * for the next child's command.
+     * Each child also uses the dup2 command to open a connection via the pipe, which are then used as a way to receive their in/output to others.*/
     pid_t pid = fork();
     if (pid == -1) {
         perror("fork() error");
@@ -312,21 +330,19 @@ void executeThreeCmds(char **cmd[], int *words, int background) {
         freeCommands(cmd, words, 2);
         exit(1);
     } else if (pid == 0) {
-        if(background==1)
-            printf("\n%d\n",getpid());
         if (dup2(fd[1], STDOUT_FILENO) == -1) {
+            close(fd[0]); /*--*/ close(fd[1]); /*--*/ close(fd[2]);/*--*/close(fd[3]);
             perror("dup failure");
             freeCommands(cmd, words, 3);
             exit(1);
         }
-        if (close(fd[0]) || close(fd[1]) || close(fd[2]) || close(fd[3])) {
+        if (close(fd[0]) == -1 || close(fd[1]) == -1 || close(fd[2]) == -1 || close(fd[3]) == -1) {
             perror("close failure");
             freeCommands(cmd, words, 3);
             exit(1);
         }
         if (execvp(*cmd[0], cmd[0]) == -1) {
             perror("command not found");
-            freeCommands(cmd, words, 3);
             exit(1);
         }
         exit(0);
@@ -342,7 +358,7 @@ void executeThreeCmds(char **cmd[], int *words, int background) {
                 close(fd[0]); /*--*/ close(fd[1]); /*--*/ close(fd[2]);/*--*/close(fd[3]);
                 exit(1);
             }
-            if (close(fd[0]) || close(fd[1]) || close(fd[2]) || close(fd[3])) {
+            if (close(fd[0]) == -1 || close(fd[1]) == -1 || close(fd[2]) == -1 || close(fd[3]) == -1) {
                 perror("close failure");
                 freeCommands(cmd, words, 3);
                 exit(1);
@@ -361,12 +377,12 @@ void executeThreeCmds(char **cmd[], int *words, int background) {
                 exit(1);
             } else if (pid == 0) {
                 if (dup2(fd[2], STDIN_FILENO) == -1) {
-                    if (close(fd[0]) || close(fd[1]) || close(fd[2]) || close(fd[3]))
-                        perror("close failure");
+                    close(fd[0]); /*--*/ close(fd[1]); /*--*/ close(fd[2]);/*--*/close(fd[3]);
+                    perror("close failure");
                     freeCommands(cmd, words, 3);
                     exit(1);
                 }
-                if (close(fd[0]) || close(fd[1]) || close(fd[2]) || close(fd[3])) {
+                if (close(fd[0]) == -1 || close(fd[1]) == -1 || close(fd[2]) == -1 || close(fd[3]) == -1) {
                     perror("close failure");
                     freeCommands(cmd, words, 3);
                     exit(1);
@@ -377,24 +393,22 @@ void executeThreeCmds(char **cmd[], int *words, int background) {
             }
         }
     }
-    if (close(fd[0]) || close(fd[1]) || close(fd[2]) || close(fd[3])) {
+    if (close(fd[0]) == -1 || close(fd[1]) == -1 || close(fd[2]) == -1 || close(fd[3]) == -1) {
         perror("close failure");
         freeCommands(cmd, words, 3);
         exit(1);
     }
+    //If the initial input was received normally with nohup and no ampersand then the father is told to wait for all three children processes
     if(background==0){
-        wait(&status);
-        wait(&status);
-        wait(&status);
-    }
-    else{
-        waitpid(-1,&status,0);
-        waitpid(-1,&status,0);
-        waitpid(-1,&status,WNOHANG);
+        for(int i=0;i<3;i++)
+            wait(&status);
     }
 }
 
-//Main loop function to keep asking user for input and call other functions according to what is passed into the stdin stream
+/*
+ * Main loop function to keep asking user for input and call other functions according to what is passed into the stdin stream
+ * while also checking if the input passed is valid
+ * */
 void loop() {
     signal(SIGCHLD,handler);
     char location[PATH_LENGTH];
@@ -434,6 +448,8 @@ void loop() {
  * If one of the commands (done/history/cd) were entered with no other words then the program either terminates/prints out all previously entered commands, or a cd error
  * If the input wasn't any of said commands, then the program will treat the input as a shell command and create an array of pointers of returned size from counter+1 and this array
  * is sent to the executeCommands function.
+ * Update: if nohup was entered at the beginning of the input then the command continues to run after the program terminates,
+ * and if an ampersand '&' was entered at the ending of a command then the command will run in the background.
 */
 void checkInput(FILE *file, char *input, size_t i, int fromHistory) {
     if (strncmp(input, EXIT, EXIT_LENGTH) == 0 && input[EXIT_LENGTH] == '\n') {
@@ -453,15 +469,21 @@ void checkInput(FILE *file, char *input, size_t i, int fromHistory) {
         return;
     } else if (input[strlen(input) - 2] == '&') {
         fromHistory == 0 ? fprintf(file, "%s", input) : fprintf(file, "%s\n", input);
+        //add the input received into the history file then remove the ampersand from input and any spaces before it
         size_t j = strlen(input)-3;
-        while(input[j]==' ')
+        while(input[j]==' ' && j>=0)
             j--;
+        if(j==-1){
+            fprintf(stderr,"Input with only an ampersand is not valid!\n");
+            return;
+        }
         input[j+1]='\n';
         input[j+2]='\0';
         counter(input, i, 1);
     }
     else if(strncmp(input,NOHUP,strlen(NOHUP))==0){
         fromHistory == 0 ? fprintf(file, "%s", input) : fprintf(file, "%s\n", input);
+        //add the input received into the history file then remove the nohup from input and any spaces after it
         size_t j = strlen(NOHUP);
         while(input[j]==' ') {
             j++;
@@ -487,6 +509,9 @@ void counter(const char *line, size_t i, int background) {
     int pipeIdx[2] = {-1, -1};
     int curPipe = 0;
     int j = 0;
+    /*
+     * this loop is used to calculate how many words each command seperated by a pipe '|' and also counts how many pipes
+     * are in the input while also saving the index of each pipe */
     while (line[i] != '\n') {
         if (line[i] == '|') {
             pipeIdx[curPipe++] = (int) i;
@@ -501,10 +526,10 @@ void counter(const char *line, size_t i, int background) {
         }
         i++;
     }
-    numberOfPipes += curPipe;
-    numberOfCommands++;
-    for (int c = 0; c < curPipe + 1; c++)
+    for (int c = 0; c < curPipe + 1; c++){
+        numberOfCommands++;
         totalNumberOfWords += wordsAmount[c];
+    }
     cmdSplitter(line, curPipe, wordsAmount, pipeIdx, background);
 }
 
